cecfb2c1f4e77f4563d9ca5050ea5867eb17a2a0
Jono <jono.nicholas@hotmail.co.uk>
2020-11-01 09:29:39 +0000

    Changed flaskr to wuxia

--- wuxia/auth.py
+++ wuxia/auth.py
@@ -2,7 +2,7 @@ import functools
 from flask import Blueprint, flash, g, render_template, request, session
 from flask import url_for, redirect
 from werkzeug.security import check_password_hash, generate_password_hash
-from flaskr.db import get_db
+from wuxia.db import get_db
 
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
