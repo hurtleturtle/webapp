cc6009feac216de2856a005eed578211d152871b
Jono <jono.nicholas@hotmail.co.uk>
2020-11-03 19:50:10 +0000

    Added autocomplete=off

--- wuxia/forms.py
+++ wuxia/forms.py
@@ -1,5 +1,5 @@
 def gen_form_item(id, name=None, placeholder=None, required=False,
-                  item_type='text', value=''):
+                  item_type='text', value='', autocomplete='off'):
     item = {
         'id': id,
         'name': name if name else id,
@@ -7,6 +7,7 @@ def gen_form_item(id, name=None, placeholder=None, required=False,
         'required': required,
         'type': item_type,
         'value': value,
+        'autocomplete': autocomplete,
     }
 
     return item
