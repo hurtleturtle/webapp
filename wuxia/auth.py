019b205179497ad5e1f5ab5da4798fb75d313605
Jono <jono.nicholas@hotmail.co.uk>
2020-11-05 13:21:04 +0000

    Go to story.list after login

--- wuxia/auth.py
+++ wuxia/auth.py
@@ -55,7 +55,7 @@ def login():
         if error is None:
             session.clear()
             session['user_id'] = user['id']
-            return redirect(url_for('index'))
+            return redirect(url_for('story.list'))
 
         flash(error)
     return render_template('auth/login.html',
