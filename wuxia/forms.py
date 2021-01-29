c3bffe85e80f4d3829501498514e7ee0da30cb48
Jono Nicholas <jono.nicholas@hotmail.co.uk>
2021-01-30 01:08:26 +0200

    Added href and removed placeholder default

--- wuxia/forms.py
+++ wuxia/forms.py
@@ -2,14 +2,14 @@ def gen_form_item(id, name=None, placeholder=None,
                   required=False, item_type='text', autocomplete='off',
                   field_type='input', label='', options={}, value='',
                   selected_option='', item_class='', label_class='',
-                  field_class=''):
+                  field_class='', href=''):
     item = {
         'id': id,
         'name': name if name else id,
         'item_class': item_class,
         'label_class': label_class,
         'field_class': field_class,
-        'placeholder': placeholder if placeholder else id,
+        'placeholder': placeholder if placeholder else '',
         'required': required,
         'type': item_type,
         'value': value,
@@ -18,6 +18,7 @@ def gen_form_item(id, name=None, placeholder=None,
         'label': label,
         'options': options,
         'selected_option': selected_option,
+        'href': href
     }
 
     return item
