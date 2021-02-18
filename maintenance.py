b1c86a6686f67f8c93a05a157174752f53772e21
Jono Nicholas <jono.nicholas@hotmail.co.uk>
2021-02-18 16:35:29 +0000

    Added Story class and arguments

--- maintenance.py
+++ maintenance.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 import sqlite3
 import pandas as pd
-import sys
+import lipsum
+from bs4 import BeautifulSoup
 from argparse import ArgumentParser
+import os
+from wuxia.story import add_story_to_db, add_chapters_to_db
 
 
 def connect(path):
@@ -51,6 +54,66 @@ class Database:
         self.commit()
 
 
+class Story:
+    def __init__(self, title, chapters=10, template_path=None):
+        self.title = title
+        self.chapters = chapters
+        self.template_path = template_path if template_path else 'wuxia/templates/story/story.html'
+        self.html = self.create_story()
+        self.generate_chapters(self.chapters)
+        self.filepath = self._save_story()
+
+    def create_story(self):
+        with open(self.template_path) as f:
+            new_story = BeautifulSoup(f.read())
+
+        new_story.title.string = self.title
+        return new_story
+
+    def _add_chapter(self, index, paragraphs=4):
+        soup = self.html
+        div = soup.new_tag('div')
+        heading = soup.new_tag('h2')
+
+        div['class'] = 'chapter'
+        heading['class'] = 'chapter-heading'
+        heading.string = 'Chapter ' + str(index)
+        div.append(heading)
+
+        for paragraph in range(paragraphs):
+            p = soup.new_tag('p')
+            p.text = lipsum.generate_paragraphs(1)
+            div.append(p)
+
+        soup.body.append(div)
+
+    def _save_story(self, filepath=None):
+        filepath = filepath if filepath else os.path.join('instance/stories', self.title.replace(' ', '_'))
+        with open(filepath, 'w') as f:
+            f.write(self.html.prettify())
+        return filepath
+
+    def generate_chapters(self, chapters):
+        for chapter in range(1, chapters + 1):
+            self._add_chapter(chapter)
+
+    def add_to_db(self):
+        """
+        Add story and chapters to database
+        :rtype: bool
+        """
+        wuxia_db = Database()
+        story = add_story_to_db(wuxia_db)
+        if not story:
+            print('Story {} could not be added to database using {}.'.format(self.title, self.filepath))
+            return False
+        else:
+            add_chapters_to_db(wuxia_db, self.filepath, story['id'], 'div.chapter', 'div.chapter > h2.chapter-heading')
+            os.remove(self.filepath)
+            return True
+
+
+
 def get_args():
     parser = ArgumentParser()
     parser.add_argument('-a', '--admin', type=int, default=None,
@@ -63,6 +126,9 @@ def get_args():
                         help='Approve story access')
     parser.add_argument('--remove', action='store_true',
                         help='Remove story access')
+    parser.add_argument('-s', '--story', action='store_true', help='Generate lorem ipsum story')
+    parser.add_argument('-c', '--chapters', type=int, default=5, help='Number of chapters to generate')
+    parser.add_argument('-t', '--title', default=lipsum.generate_words(1), help='Story title')
 
     return parser.parse_args()
 
@@ -85,3 +151,7 @@ if __name__ == '__main__':
 
     if args.list:
         print(db.get_users())
+
+    if args.story:
+        s = Story(title=args.title, chapters=args.chapters)
+        s.add_to_db()
