8e9740fd7da72519a2f0443ca01ac3da652d402d
Jono Nicholas <jono.nicholas@hotmail.co.uk>
2021-02-22 15:18:50 +0000

    Added template folder
    Signed-off-by: Jono Nicholas <jono.nicholas@hotmail.co.uk>

--- wuxia/misc.py
+++ wuxia/misc.py
@@ -2,7 +2,7 @@ from flask import Blueprint, request, render_template, render_template_string
 from wuxia.auth import admin_required
 
 
-bp = Blueprint('misc', __name__)
+bp = Blueprint('misc', __name__, template_folder='templates/misc')
 
 
 @bp.route('/cookies', methods=['GET'])
@@ -14,7 +14,7 @@ def cookies():
     else:
         returned_cookies = {'naughty': 'cookie'}
 
-    return render_template('misc/cookies.html', cookies=returned_cookies)
+    return render_template('cookies.html', cookies=returned_cookies)
 
 
 @bp.route('/template-injection', methods=['GET', 'POST'])
@@ -23,6 +23,8 @@ def inject():
     if request.method == 'POST':
         template_injection = request.form['injection']
         result = render_template_string(template_injection)
-        return render_template('misc/injection.html', result=result)
+        return render_template('injection.html', result=result)
+
+    return render_template('injection.html')
 
     return render_template('misc/injection.html')
