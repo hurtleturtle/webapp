4577d03dcbb73a059c2b5bc968992de454e8e1c5
Jono Nicholas <jono.nicholas@hotmail.co.uk>
2021-02-26 23:39:17 +0000

    Added test_challenge
    Signed-off-by: Jono Nicholas <jono.nicholas@hotmail.co.uk>

--- wuxia/challenges.py
+++ wuxia/challenges.py
@@ -2,20 +2,20 @@ from flask import Blueprint, render_template
 
 
 bp = Blueprint('challenges', __name__, url_prefix='/challenges', template_folder='templates/challenges')
+test_challenge = {
+    'challenge_id': 1,
+    'title': 'Test Challenge',
+    'short_description': 'A naughty test challenge'
+}
 
 
 @bp.route('/', methods=['GET'])
 def show_all():
-    chals = [
-        {
-            'challenge_id': 1,
-            'title': 'Test Challenge',
-            'short_description': 'A naughty test challenge'
-        }
-    ]
+    chals = [test_challenge]
     return render_template('list.html', challenges=chals)
 
 
 @bp.route('/<int:challenge_id>', methods=['GET'])
 def show_challenge(challenge_id):
-    pass
+    challenge = test_challenge
+    return render_template('description.html', challenge=challenge)
