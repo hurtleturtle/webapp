a36174ca4ba92acf9bce71bfb53712a288fc2514
Jono Nicholas <jono.nicholas@hotmail.co.uk>
2021-02-26 22:30:04 +0000

    Added challenges.show_all
    Signed-off-by: Jono Nicholas <jono.nicholas@hotmail.co.uk>

--- wuxia/challenges.py
+++ wuxia/challenges.py
@@ -5,5 +5,17 @@ bp = Blueprint('challenges', __name__, url_prefix='/challenges', template_folder
 
 
 @bp.route('/', methods=['GET'])
-def show():
+def show_all():
+    chals = [
+        {
+            'challenge_id': 1,
+            'title': 'Test Challenge',
+            'short_description': 'A naughty test challenge'
+        }
+    ]
+    return render_template('list.html', challenges=chals)
+
+
+@bp.route('/<int:challenge_id>', methods=['GET'])
+def show_challenge(challenge_id):
     pass
