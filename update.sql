c645ee65fc8f012a543bcc6a9cf0b97a0b50feb4
Jono Nicholas <jono.nicholas@hotmail.co.uk>
2021-02-28 22:45:43 +0000

    Updated to create new challenges tables
    Signed-off-by: Jono Nicholas <jono.nicholas@hotmail.co.uk>

--- update.sql
+++ update.sql
@@ -1,7 +1,22 @@
-DROP TABLE IF EXISTS challenges;
 CREATE TABLE challenges (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     title TEXT NOT NULL,
-    short_description TEXT NOT NULL
+    short_description TEXT NOT NULL,
+);
+
+CREATE TABLE challenge_descriptions (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    challenge_id INTEGER NOT NULL,
+    sequence_num INTEGER NOT NULL,
+    description TEXT,
+    FOREIGN KEY (challenge_id) REFERENCES challenges (id)
+);
+
+CREATE TABLE challenge_files (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    challenge_id INTEGER NOT NULL,
+    type TEXT NOT NULL,
+    file_name TEXT NOT NULL,
+    FOREIGN KEY (challenge_id) REFERENCES challenges (id)
 );
