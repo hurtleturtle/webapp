80496b51e2f182c43dafddd4c865c45f058e1d60
Jono Nicholas <jono.nicholas@hotmail.co.uk>
2021-02-28 14:35:27 +0000

    Added add route
    Signed-off-by: Jono Nicholas <jono.nicholas@hotmail.co.uk>

--- wuxia/challenges.py
+++ wuxia/challenges.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+from wuxia.auth import write_admin_required
+from wuxia.forms import gen_form_item
 
 
 bp = Blueprint('challenges', __name__, url_prefix='/challenges', template_folder='templates/challenges')
@@ -20,3 +22,34 @@ def show_all():
 def show_challenge(challenge_id):
     challenge = test_challenge
     return render_template('description.html', challenge=challenge)
+
+
+@bp.route('/add', methods=['GET', 'POST'])
+@write_admin_required
+def add():
+    if request.method == 'POST':
+
+        return 'Posted'
+
+    groups = {
+        'details': {
+            'group_title': 'Add New Challenge',
+            'challenge_title': gen_form_item('title', placeholder='Title', required=True),
+            'challenge_description': gen_form_item('description', placeholder='Description', required=True,
+                                                   field_class='challenge-long-description', field_type='textarea'),
+        },
+        'samples': {
+            'group_title': 'Sample Files',
+            'sample': gen_form_item('sample_files', item_type='file', multiple=True),
+        },
+        'verification': {
+            'group_title': 'Verification Files',
+            'verification': gen_form_item('verification_files', item_type='file', multiple=True)
+        },
+        'submit': {
+            'button': gen_form_item('btn-submit', item_type='submit',
+                                    value='Add')
+        },
+    }
+    
+    return render_template('add.html', form_groups=groups)
