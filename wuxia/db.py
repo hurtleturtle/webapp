00ed40c0957699e3052503bc1146f1da440c5f42
Jono Nicholas <jono.nicholas@hotmail.co.uk>
2021-02-28 09:56:54 +0000

    Added Database class to db.py
    Signed-off-by: Jono Nicholas <jono.nicholas@hotmail.co.uk>

--- wuxia/db.py
+++ wuxia/db.py
@@ -4,20 +4,65 @@ from flask import current_app, g
 from flask.cli import with_appcontext
 
 
+def connect(path):
+    conn = sqlite3.connect(path, detect_types=sqlite3.PARSE_DECLTYPES)
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+class Database:
+    def __init__(self, path=None):
+        path = path if path else current_app.config['DATABASE']
+        self.db = connect(path)
+        self.execute = self.db.execute
+        self.commit = self.db.commit
+        self.close = self.db.close
+        self.executemany = self.db.executemany
+        self.executescript = self.db.executescript
+
+    def make_admin(self, uid, admin_level=0):
+        levels = {0: 'no', 1: 'read', 2: 'read-write'}
+        query = 'UPDATE users SET admin = ? WHERE id = ?'
+        params = (levels[admin_level], uid)
+        self.execute(query, params)
+        self.commit()
+
+    def get_users(self):
+        users = self.execute('SELECT * FROM users').fetchall()
+        if users:
+            return pd.DataFrame(users, columns=users[0].keys())
+        else:
+            return pd.DataFrame()
+
+    def get_user(self, uid=None, name=None):
+        query = 'SELECT * FROM users WHERE '
+        params = tuple()
+
+        if uid:
+            query += 'id = ?'
+            params = (uid,)
+        elif name:
+            query += 'username = ?'
+            params = (name,)
+
+        return self.execute(query, params).fetchone()
+
+    def set_story_access(self, uid, access=True):
+        query = 'UPDATE users SET access_approved = ? WHERE id = ?'
+        params = (int(access), uid)
+        self.execute(query, params)
+        self.commit()
+
+
 def get_db():
     if 'db' not in g:
-        g.db = sqlite3.connect(
-            current_app.config['DATABASE'],
-            detect_types=sqlite3.PARSE_DECLTYPES,
-        )
-        g.db.row_factory = sqlite3.Row
+        g.db = Database()
 
     return g.db
 
 
 def close_db(e=None):
     db = g.pop('db', None)
-
     if db is not None:
         db.close()
 
