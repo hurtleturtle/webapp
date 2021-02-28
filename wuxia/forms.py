4e44110e51d0f3c74cd76c0711977c1041b3387c
Jono Nicholas <jono.nicholas@hotmail.co.uk>
2021-02-28 12:15:16 +0000

    Added textarea to form
    Signed-off-by: Jono Nicholas <jono.nicholas@hotmail.co.uk>

--- wuxia/forms.py
+++ wuxia/forms.py
@@ -2,7 +2,7 @@ def gen_form_item(item_id=None, name=None, placeholder=None,
                   required=False, item_type='text', autocomplete='off',
                   field_type='input', label='', options={}, value='',
                   selected_option='', item_class='', label_class='',
-                  field_class='', href='', multiple=False):
+                  field_class='', href='', multiple=False, rows=10, columns=80):
     item = {
         'id': item_id,
         'name': name if name else item_id,
@@ -19,7 +19,9 @@ def gen_form_item(item_id=None, name=None, placeholder=None,
         'options': options,
         'selected_option': selected_option,
         'href': href,
-        'multiple': multiple
+        'multiple': multiple,
+        'rows': rows,
+        'columns': columns
     }
 
     return item
