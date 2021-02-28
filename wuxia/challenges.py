02417376455239b7440ff61149da3a51a7bd5733
Jono Nicholas <jono.nicholas@hotmail.co.uk>
2021-02-28 20:03:04 +0000

    Added short_description, long_description and results files
    Signed-off-by: Jono Nicholas <jono.nicholas@hotmail.co.uk>

--- wuxia/challenges.py
+++ wuxia/challenges.py
@@ -32,18 +32,21 @@ def add():
     if request.method == 'POST':
         form = request.form
         title = escape(form.get('title'))
-        description = escape(form.get('description'))
+        short_description = escape(form.get('short_description'))
+        long_description = escape(form.get('long_description'))
         samples = request.files.getlist('sample_files')
         verifiers = request.files.getlist('verification_files')
         db = get_db()
 
-        return title + description + '\n' + str(samples)
+        return ''
 
     groups = {
         'details': {
             'group_title': 'Add New Challenge',
             'challenge_title': gen_form_item('title', placeholder='Title', required=True),
-            'challenge_description': gen_form_item('description', placeholder='Description', required=True,
+            'challenge_short_description': gen_form_item('short_description', placeholder='Short Description',
+                                                         required=True),
+            'challenge_description': gen_form_item('long_description', placeholder='Description', required=True,
                                                    field_class='challenge-long-description', field_type='textarea'),
         },
         'samples': {
@@ -53,6 +56,10 @@ def add():
         'verification': {
             'group_title': 'Verification Files',
             'verification': gen_form_item('verification_files', item_type='file', multiple=True)
+        },
+        'results': {
+            'group_title': 'Results Files',
+            'results': gen_form_item('results_files', item_type='file', multiple=True)
         },
         'submit': {
             'button': gen_form_item('btn-submit', item_type='submit',
