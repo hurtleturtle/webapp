9a55ab0ca8d11d83fac5c7107565e687ea10ef6c
Jono Nicholas <jono.nicholas@hotmail.co.uk>
2021-03-01 08:07:06 +0000

    Added challenge_answers table
    Signed-off-by: Jono Nicholas <jono.nicholas@hotmail.co.uk>

--- update.sql
+++ update.sql
@@ -1,22 +1,8 @@
-CREATE TABLE challenges (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    title TEXT NOT NULL,
-    short_description TEXT NOT NULL
-);
-
-CREATE TABLE challenge_descriptions (
+CREATE TABLE challenge_answers (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     challenge_id INTEGER NOT NULL,
-    sequence_num INTEGER NOT NULL,
-    description TEXT,
-    FOREIGN KEY (challenge_id) REFERENCES challenges (id)
-);
-
-CREATE TABLE challenge_files (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    challenge_id INTEGER NOT NULL,
-    type TEXT NOT NULL,
-    file_name TEXT NOT NULL,
-    FOREIGN KEY (challenge_id) REFERENCES challenges (id)
+    user_id INTEGER NOT NULL,
+    attempt INTEGER NOT NULL DEFAULT 1,
+    pass BOOLEAN NOT NULL DEFAULT false,
+    evaluation_result TEXT
 );
