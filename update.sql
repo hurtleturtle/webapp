97be0dca302303a9b14998a5dbb86e837b1cf12a
Jono Nicholas <jono.nicholas@hotmail.co.uk>
2021-03-01 15:06:02 +0000

    Added submission columns to users table
    Signed-off-by: Jono Nicholas <jono.nicholas@hotmail.co.uk>

--- update.sql
+++ update.sql
@@ -1,3 +1,5 @@
+DROP TABLE IF EXISTS challenge_answers;
+
 CREATE TABLE challenge_answers (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     challenge_id INTEGER NOT NULL,
@@ -6,3 +8,6 @@ CREATE TABLE challenge_answers (
     pass BOOLEAN NOT NULL DEFAULT false,
     evaluation_result TEXT
 );
+
+ALTER TABLE users ADD COLUMN submission_approved BOOLEAN NOT NULL DEFAULT false;
+ALTER TABLE users ADD COLUMN submission_approval_requested BOOLEAN NOT NULL DEFAULT false;
