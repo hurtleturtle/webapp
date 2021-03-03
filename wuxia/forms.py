ca373012b033b281a023a994d10ccc16bdae02b4
Jono Nicholas <jono.nicholas@hotmail.co.uk>
2021-03-03 22:04:40 +0000

    Added capability to set textarea_text
    Signed-off-by: Jono Nicholas <jono.nicholas@hotmail.co.uk>

--- wuxia/forms.py
+++ wuxia/forms.py
@@ -2,7 +2,7 @@ def gen_form_item(item_id=None, name=None, placeholder=None,
                   required=False, item_type='text', autocomplete='off',
                   field_type='input', label='', options={}, value='',
                   selected_option='', item_class='', label_class='',
-                  field_class='', href='', multiple=False, rows=10, columns=80):
+                  field_class='', href='', multiple=False, rows=10, columns=80, textarea_text=None):
     item = {
         'id': item_id,
         'name': name if name else item_id,
@@ -21,7 +21,8 @@ def gen_form_item(item_id=None, name=None, placeholder=None,
         'href': href,
         'multiple': multiple,
         'rows': rows,
-        'columns': columns
+        'columns': columns,
+        'textarea_text': textarea_text
     }
 
     return item
