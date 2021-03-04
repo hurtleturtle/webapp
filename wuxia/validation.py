9baa204defd7ef335722010b51f644e70c9f93f0
Jono Nicholas <jono.nicholas@hotmail.co.uk>
2021-03-04 20:23:22 +0000

    Added validation of user's code
    Signed-off-by: Jono Nicholas <jono.nicholas@hotmail.co.uk>

--- wuxia/validation.py
+++ wuxia/validation.py
@@ -1,19 +1,46 @@
 from wuxia.db import get_db
+from flask import current_app
+from flask.cli import with_appcontext
+import click
 import docker
 from docker.errors import ImageNotFound
+from jinja2 import Template
+import os
 
 
 client = docker.from_env()
 
 
-def check_docker_images(image_name='validator:1', path='.', dockerfile='Dockerfile'):
+def check_docker_images(image_name='validator:latest', path=None, dockerfile_template=None):
     image = None
+    dockerfile = 'Dockerfile'
+    dockerfile_template = dockerfile_template if dockerfile_template else 'Dockerfile.j2'
+
     try:
         image = client.images.get(image_name)
+        print(image)
     except ImageNotFound:
-        image = client.images.build(path=path, tag=image_name, rm=True, dockerfile=dockerfile)
-    finally:
-        return image
+        with open(dockerfile_template) as f:
+            template = Template(f.read())
+
+        completed_template = template.render(uid=str(os.getuid()))
+        print(completed_template)
+        with open(dockerfile, 'w') as f:
+            f.write(completed_template)
+
+        path = path if path else os.getcwd()
+        image, logs = client.images.build(path=path, tag=image_name, rm=True, dockerfile=dockerfile)
+        print(f'{image} built.')
+
+    return image
+
+
+def generate_entrypoint(user_id, verifier_filename, template='validate.j2', destination='validate.sh'):
+    with open(template) as f1, open(destination, 'w') as f2:
+        entrypoint = Template(f1.read())
+        f2.write(entrypoint.render(user_id=user_id, verifier_filename=verifier_filename))
+
+    os.chmod(destination, 0o755)
 
 
 class Validator:
@@ -21,8 +48,37 @@ class Validator:
         db = get_db()
         self.challenge_id = challenge_id
         self.user_id = user_id
-        self.image = check_docker_images()
+        self.verifier_filename = db.get_challenges(challenge_id, ['verifier_filename'])[0]['verifier_filename']
         self.verifiers = db.get_challenge_files(challenge_id, file_types=['verifier'], columns=['file_name'])
         self.results = db.get_challenge_files(challenge_id, file_types=['result'], columns=['file_name'])
+        self.user_file = db.get_challenge_files(challenge_id, user_id=user_id, file_types=['user'],
+                                                columns=['file_name'])
+        self.verification_script = 'validate.sh'
+        generate_entrypoint(self.user_id, self.verifier_filename, destination=self.verification_script)
+        self.image = check_docker_images()
 
     # TODO: generate output from user code with JSONs and compare to expected output text
+    @with_appcontext
+    def generate_user_output(self,  image=None):
+        image = image if image else self.image
+        challenge_folder = os.path.join(current_app.instance_path, 'challenges/', str(self.challenge_id))
+        volumes = {
+            challenge_folder: {
+                'bind': '/tmp/validation/files',
+                'mode': 'rw'
+            }
+        }
+
+        results = client.containers.run(image.tags[0], f'{self.verification_script}', volumes=volumes).decode()
+        return results
+
+
+@click.command('check-validator')
+@with_appcontext
+def check_validator():
+    v = Validator(1, 1)
+    print(v.generate_user_output())
+
+
+def init_app(app):
+    app.cli.add_command(check_validator)
