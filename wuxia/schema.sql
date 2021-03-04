6ec6c8dbe74adfa47d85a1271ba80ec0c1c641af
Jono Nicholas <jono.nicholas@hotmail.co.uk>
2021-03-04 20:24:18 +0000

    Added verifier_filename to challenges table
    Signed-off-by: Jono Nicholas <jono.nicholas@hotmail.co.uk>

--- wuxia/schema.sql
+++ wuxia/schema.sql
@@ -44,7 +44,8 @@ CREATE TABLE challenges (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     title TEXT NOT NULL,
-    short_description TEXT NOT NULL
+    short_description TEXT NOT NULL,
+    verifier_filename TEXT NOT NULL
 );
 
 CREATE TABLE challenge_descriptions (
