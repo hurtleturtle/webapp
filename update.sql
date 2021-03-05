1a9065c9116507adc580c33ce1aff3b349174881
Jono Nicholas <jono.nicholas@hotmail.co.uk>
2021-03-05 14:07:57 +0000

    Added SQL updates to update.sql
    Signed-off-by: Jono Nicholas <jono.nicholas@hotmail.co.uk>

--- update.sql
+++ update.sql
@@ -1,4 +1,19 @@
-DROP TABLE IF EXISTS challenge_answers;
+ALTER TABLE challenge_files RENAME TO challenge_files_old;
+
+CREATE TABLE challenge_files (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	challenge_id INTEGER NOT NULL,
+	user_id INTEGER DEFAULT 0,
+	type TEXT NOT NULL,
+	file_name TEXT NOT NULL,
+	FOREIGN KEY (challenge_id) REFERENCES users (id)
+);
+
+INSERT INTO challenge_files SELECT * FROM challenge_files_old;
+
+ALTER TABLE challenges ADD COLUMN verifier_filename TEXT NOT NULL;
+ALTER TABLE users ADD COLUMN submission_approved BOOLEAN NOT NULL DEFAULT false;
+ALTER TABLE users ADD COLUMN submission_approval_requested BOOLEAN NOT NULL DEFAULT false;
 
 CREATE TABLE challenge_answers (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -8,6 +23,3 @@ CREATE TABLE challenge_answers (
     pass BOOLEAN NOT NULL DEFAULT false,
     evaluation_result TEXT
 );
-
-ALTER TABLE users ADD COLUMN submission_approved BOOLEAN NOT NULL DEFAULT false;
-ALTER TABLE users ADD COLUMN submission_approval_requested BOOLEAN NOT NULL DEFAULT false;
