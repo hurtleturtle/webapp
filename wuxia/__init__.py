2a93bfc96f43b2dac7b959751440417cc37f3192
Jono Nicholas <jono.nicholas@hotmail.co.uk>
2021-06-23 16:57:21 +0100

    Added routes template folder

--- wuxia/__init__.py
+++ wuxia/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 
 
 def create_app(test_config=None):
-    app = Flask(__name__, instance_relative_config=True)
+    app = Flask(__name__, instance_relative_config=True, template_folder='routes/templates')
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'wuxia.sqlite'),
