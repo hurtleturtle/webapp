529e070aa1da842aa9a56ac31440f13ed5f4d7eb
Jono Nicholas <jono.nicholas@bt.com>
2021-07-15 16:33:51 +0100

    Removed extra quotes

--- scripts/install_db.sh
+++ scripts/install_db.sh
@@ -5,7 +5,7 @@ apt-get install mysql-server
 
 echo 'Preparing config...'
 read -s -p 'Database password: ' PASSWORD
-sed -E 's/<password>/'"$PASSWORD"'/' init_template.sql > init.sql
+sed -E 's/<password>/'$PASSWORD'/' init_template.sql > init.sql
 
 echo 'Initialising database...'
 mysql < init.sql
