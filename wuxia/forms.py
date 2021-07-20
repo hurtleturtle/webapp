b3f7fc4408d6ac1e639a23b75d7b1a6a2c910ec6
Jono Nicholas <jono.nicholas@bt.com>
2021-07-20 17:44:45 +0100

    Added capability to add extra attributes

--- wuxia/forms.py
+++ wuxia/forms.py
@@ -2,7 +2,8 @@ def gen_form_item(item_id=None, name=None, placeholder=None,
                   required=False, item_type='text', autocomplete='off',
                   field_type='input', label='', options={}, value='',
                   selected_option='', item_class='', label_class='',
-                  field_class='', href='', multiple=False, rows=10, columns=80, textarea_text=None):
+                  field_class='', href='', multiple=False, rows=10, columns=80, 
+                  textarea_text=None, extra_attrs=None):
     item = {
         'id': item_id,
         'name': name if name else item_id,
@@ -22,7 +23,8 @@ def gen_form_item(item_id=None, name=None, placeholder=None,
         'multiple': multiple,
         'rows': rows,
         'columns': columns,
-        'textarea_text': textarea_text
+        'textarea_text': textarea_text,
+        'extra_attrs': extra_attrs
     }
 
     return item
