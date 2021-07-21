3629b053efa30dcd19633ae14f054c73d7434b33
Jono Nicholas <jono.nicholas@bt.com>
2021-07-21 12:40:46 +0100

    Added query capability

--- wuxia/routes/misc.py
+++ wuxia/routes/misc.py
@@ -1,6 +1,9 @@
 from flask import Blueprint, request, render_template, render_template_string
-from wuxia.routes.auth import admin_required
-
+from wuxia.routes.auth import admin_required, write_admin_required
+from wuxia.forms import gen_form_item
+from wuxia.db import get_db
+from mysql.connector import Error as SQLError
+from pandas import DataFrame
 
 bp = Blueprint('misc', __name__, template_folder='templates/misc')
 
@@ -31,3 +34,37 @@ def inject():
 @bp.route('/bots', methods=['GET'])
 def bots():
     return render_template('bots.html')
+
+
+@bp.route('/query', methods=['GET', 'POST'])
+@write_admin_required
+def query():
+    groups = {
+        'query': {
+            'group_title': 'SQL Query',
+            'query_string': gen_form_item('query', name='query', placeholder='Type SQL query here (no commits)', field_type='textarea')
+        },
+        'submit': {
+            'btn-submit': gen_form_item('btn-submit', item_type='submit', value='Submit')
+        }
+    }
+
+    if request.method == 'POST':
+        query = request.form.get('query')
+        db = get_db()
+        result = ''
+
+        try:
+            db.execute(query)
+            result = QueryResult(db.cursor.fetchall()).to_html()
+        except SQLError as e:
+            result = f'SQL error encountered:<br />{e}'
+        
+        return render_template('misc/query.html', form_groups=groups, result=result)
+
+    return render_template('misc/query.html', form_groups=groups)
+
+
+class QueryResult(DataFrame):
+    def __bool__(self):
+        return self.empty
