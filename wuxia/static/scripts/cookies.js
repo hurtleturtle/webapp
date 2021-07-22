94db96b14222aaddd54af843613af472e4990292
Jono Nicholas <jono.nicholas@bt.com>
2021-07-22 16:12:40 +0100

    Added semicolons

--- wuxia/static/scripts/cookies.js
+++ wuxia/static/scripts/cookies.js
@@ -1,9 +1,9 @@
 window.onload = function() {
-    fill_browser_cookies_into_table()
+    fill_browser_cookies_into_table();
 }
 
 function fill_browser_cookies_into_table(params) {
-    var browser_cookie_cells = document.querySelectorAll("td.browser-cookie")
+    var browser_cookie_cells = document.querySelectorAll("td.browser-cookie");
     var cookies = document.cookie.split('&');
     var cookie_dict = {};
 
@@ -14,15 +14,15 @@ function fill_browser_cookies_into_table(params) {
         
 
     for (cell_idx in browser_cookie_cells) {
-        cell = browser_cookie_cells[cell_idx]
-        cell_cookie = cell.id
-        browser_cookie_value = cookie_dict[cell_cookie]
+        cell = browser_cookie_cells[cell_idx];
+        cell_cookie = cell.id;
+        browser_cookie_value = cookie_dict[cell_cookie];
 
         if (browser_cookie_value) {
-            cell.innerHTML = browser_cookie_value
+            cell.innerHTML = browser_cookie_value;
         }
         else {
-            cell.innerHTML = 'Cookie inaccessible with JavaScript'
+            cell.innerHTML = 'Cookie inaccessible with JavaScript';
         }
     }
 }
