09f16119bdc57c3e3acc0b51140489d59aae759d
Jono Nicholas <jono.nicholas@bt.com>
2021-08-27 17:10:01 +0100

    Added debug print session variable

--- wuxia/routes/auth.py
+++ wuxia/routes/auth.py
@@ -97,6 +97,7 @@ def get_user_form(form_type):
 @bp.before_app_request
 def load_logged_in_user():
     user_id = session.get('user_id')
+    print(session)
 
     if user_id is None:
         g.user = None
