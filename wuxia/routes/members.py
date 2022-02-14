0446616aad026386fc381f372c614a175a3b356a
Jono Nicholas <jono.nicholas@hotmail.co.uk>
2022-02-14 10:44:35 +0000

    Add check in capability

--- wuxia/routes/members.py
+++ wuxia/routes/members.py
@@ -1,7 +1,23 @@
-from flask import Blueprint, request, render_template
-from wuxia.routes.auth import admin_required, write_admin_required, test_access_required
+from flask import Blueprint, request, render_template, flash
+from wuxia.routes.auth import admin_required, login_required
 from wuxia.forms import gen_form_item
 from wuxia.db import get_db
 
 
-bp = Blueprint('members', __name__, url_prefix='members', template_folder='routes/members')
+bp = Blueprint('members', __name__, url_prefix='/members', template_folder='routes/members')
+
+
+@bp.route('/check-in', methods=['GET'])
+@login_required
+def check_in_to_class():
+    db = get_db()
+    current_user = db.get_user()
+    class_id = 1
+    query = 'INSERT INTO attendance (member_id, class_id) VALUES (%s, %s);'
+    params = (current_user['id'], class_id)
+    db.execute(query, params)
+    db.commit()
+
+    flash('Checked in!')
+
+    return render_template('checkin.html')
