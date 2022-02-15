18a7c3e711e06e2659c75a9d21d913e86dad42a2
Jono Nicholas <jono.nicholas@hotmail.co.uk>
2022-02-15 09:55:49 +0000

    Added reports

--- wuxia/routes/members.py
+++ wuxia/routes/members.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request, render_template, flash
 from wuxia.routes.auth import admin_required, login_required
 from wuxia.forms import gen_form_item
-from wuxia.db import get_db
+from wuxia.db import get_db, QueryResult
 
 
 bp = Blueprint('members', __name__, url_prefix='/members', template_folder='templates/members')
@@ -27,3 +27,23 @@ def check_in_to_class():
 @admin_required
 def add_class():
     return render_template('add_class.html')
+
+
+@bp.route('/reports', methods=['GET', 'POST'])
+@admin_required
+def reports():
+    groups = {
+        'report': {
+            'group_title': 'Generate Report'
+        },
+        'submit': {
+            'btn-submit': gen_form_item('btn-submit', item_type='submit', value='Submit')
+        }
+    }
+
+    if request.method == 'POST':
+        db = get_db()
+        result = QueryResult(db.get_attendance('2022-02-14', '2022-02-20')).to_html(index=False)
+        return render_template('report.html', form_groups=groups, result=result)
+            
+    return render_template('report.html', form_groups=groups)
